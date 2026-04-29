8e98ea71f5c6218bc54a32aee04fd7bca481f03f
Eryck Menco <mencoeryck@gmail.com>
2026-04-29 16:14:19 -0500

    Nuevas configuraciones

---
+++ Js/hamburguesa.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("header-menu")
+const header = document.getElementById("header")
+
+function mostrar() {
+    header.classList.toggle("abierto")
+}
+
+button.addEventListener("click", mostrar)
